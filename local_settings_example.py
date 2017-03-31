465f2a04a09a4914c3c9641833b8beca447cfd7b
Nikolay Blokhin <nikolay@blokhin.us>
2017-03-31 12:54:22 +0200

    Add example for local_setting

---
+++ local_settings_example.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+SLACK_API_TOKEN = '...'
+SLACK_CHANNEL = 'general'
+
+TWITTER_APP_KEY = '...'
+TWITTER_APP_SECRET = '...'
+TWITTER_OAUTH_TOKEN = '...'
+TWITTER_OAUTH_TOKEN_SECRET = '...'
+
+DB_ENGINE = 'sqlite:///twi/twi_db.sqlite'
+
+SEARCH_TERM = 'sale'
+
+
